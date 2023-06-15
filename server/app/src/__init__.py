07f8e7a68d7552d84987d670e023c37bdc63be67
Will <programergeek@gmail.com>
2023-06-15 14:04:47 +0200

    server: feat: create /stop and /start endpoints

--- server/app/src/__init__.py
+++ server/app/src/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
-from .controllers.create_new_vm import create_vm  
+from .controllers.create_new_vm import create_vm
+from .controllers.stop_vm import stop_vm  
+from .controllers.start_vm import start_vm
 app = Flask(__name__)
 
 
@@ -7,10 +9,21 @@ app = Flask(__name__)
 def hello():
     return {'value': 'Hello World!'}
 
-@app.route('/new_vm', methods=["POST","GET"])
+@app.route('/new_vm', methods=["POST"])
 def new_vm():
     if request.method == "POST":
         data = request.get_json()
         return create_vm(lab_name=data['lab_name'], student_id=data['id'], static_ip=data['ip'])
-    else:
-        return "meep"
+    return {"status": 404, "message": "Page not Found"} 
+
+@app.route('/stop', methods=["POST"])
+def stop():
+    if request.method == "POST":
+        data = request.get_json()
+        return stop_vm(lab_name=data['lab_name'], student_id=data['id'])
+
+@app.route('/start', methods=["POST"])
+def start():
+    if request.method == "POST":
+        data = request.get_json()
+        return start_vm(lab_name=data['lab_name'], student_id=data['id'])
