b201a2d2dabd9c0e6eddf51f4f3ec1229fe0b0af
Will <programergeek@gmail.com>
2023-06-15 09:24:01 +0200

    script: start vm

---
+++ scripts/startvm.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+while getopts ":l:i:" OPTION; do
+    case "$OPTION" in 
+        l)
+            lab_name="$OPTARG"
+            ;;
+        i)
+            student_id="$OPTARG"
+            ;;
+        ?)
+            echo "Usage: $0 [-i student_id] [-l lab_name]"
+            exit 1
+            ;;
+    esac
+done
+
+# check if student_id is set
+if [ -z "$student_id" ]
+then
+    echo "Error: missing student id"
+    exit 1
+fi
+
+# check if student_id is set
+if [ -z "$lab_name" ]
+then
+    echo "Error: missing lab name"
+    exit 1
+fi
+
+root_dir="/home/will/Documents/Projects"
+
+export VAGRANT_CWD="$root_dir/labs/$student_id/$lab_name"
+
+vagrant up
