5989c02e16a266a0c8df5a6cd47eff716dce73f5
Will <programergeek@gmail.com>
2023-06-22 15:10:32 +0200

    feat: frontend: create custom signup & signinpages

--- frontend/src/server/auth.ts
+++ frontend/src/server/auth.ts
@@ -49,18 +49,13 @@ export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-    })
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
   ],
+  pages: {
+    signIn: "/auth/signin",
+    newUser: "/auth/signup"
+  }
 };
 
 /**
