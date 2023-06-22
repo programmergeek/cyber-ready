f8393e1687d097f359bdcbe2c589d5d97f552675
Will <programergeek@gmail.com>
2023-06-22 09:34:43 +0200

    config: frontend: tailwindcss

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -3,7 +3,24 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        deepBlack: "#101010",
+        black: "#1E1E1E",
+        darkGrey: "#333333",
+        grey: "#7D7D7D",
+        lightGrey: "#D9D9D9",
+        red: "#9B0000",
+        green: "#148C27",
+        yellow: "#F2C94C"
+      },
+      fontSize:{
+        h1: ["4rem", "4.5rem"],
+        h2: ["2.25rem", "2.75rem"],
+        h3: ["1.5rem", "2rem"],
+        regular: ["1rem", "1.5rem"]
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
