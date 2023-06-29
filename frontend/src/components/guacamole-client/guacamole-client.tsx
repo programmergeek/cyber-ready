4bb32fd2315e39fa1fbf69e78c2e80b17554dd96
Will <programergeek@gmail.com>
2023-06-29 10:58:30 +0200

    frontend: feat: get mouse and keyboard input

--- frontend/src/components/guacamole-client/guacamole-client.tsx
+++ frontend/src/components/guacamole-client/guacamole-client.tsx
@@ -36,8 +36,40 @@ const GuacamoleClient: React.FC<GuacamoleStageProps> = () => {
 
   useEffect(() => {
     if (myRef.current) {
+      myRef.current.innerHTML = "";
       if (client) {
         myRef.current.appendChild(client?.getDisplay().getElement() as Node);
+        const mouse = new Guacamole.Mouse(client.getDisplay().getElement());
+
+        // send mouse movement to guacamole api
+        mouse.onmousemove = (mouseState) => {
+          client.getDisplay().showCursor(true);
+          if (navigator.userAgent.indexOf("Firefox") === -1) {
+            mouseState.x = mouseState.x + 125;
+            mouseState.y = mouseState.y + 65;
+          }
+          client.sendMouseState(mouseState);
+        };
+
+        // send mouse down event to guacamole api
+        mouse.onmousedown = (mouseState) => {
+          client.sendMouseState(mouseState);
+        };
+
+        // send mouse up event to guacamole api
+        mouse.onmouseup = (mouseState) => {
+          client.sendMouseState(mouseState);
+        };
+
+        // hide the vm cursor outside of the display element
+        mouse.onmouseout = () => {
+          client.getDisplay().showCursor(false);
+        };
+
+        // get keyboard input
+        const keyboard = new Guacamole.Keyboard(document);
+        keyboard.onkeydown = (keysym) => client.sendKeyEvent(1, keysym);
+        keyboard.onkeyup = (keysym) => client.sendKeyEvent(0, keysym);
         client.connect("token=" + token);
       }
     }
