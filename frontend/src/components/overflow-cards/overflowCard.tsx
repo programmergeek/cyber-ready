b5981585fc40db136ebc79944962d8b6d9248365
Will <programergeek@gmail.com>
2023-07-04 11:58:42 +0200

    frontend: feat: create overflow card component

---
+++ frontend/src/components/overflow-cards/overflowCard.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Card } from "../card/card";
+
+interface OverflowCardProps {
+  title?: string;
+  children?: React.ReactNode;
+  height?: string;
+}
+
+export const OverflowCard: React.FC<OverflowCardProps> = ({ ...props }) => {
+  return (
+    <Card dark>
+      <div
+        className={`w-full rounded-meap bg-deepBlack px-4 py-3 ${
+          props.title ? "" : "hidden"
+        }`}
+      >
+        {props.title}
+      </div>
+      <div className="relative">
+        <div
+          className={`-z-10 mt-5 overflow-auto px-2 pb-2 ${
+            props.height ? `max-h-[${props.height}]` : "max-h-72"
+          }`}
+        >
+          {props.children}
+        </div>
+        <div className="absolute bottom-0 h-8 w-full bg-gradient-to-t from-black"></div>
+      </div>
+    </Card>
+  );
+};
