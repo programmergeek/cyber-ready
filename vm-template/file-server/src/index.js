e723c0c7a3f9706eeb873ab6ac54b383f37a3de7
Will <programergeek@gmail.com>
2023-07-10 11:36:20 +0200

    feat: enable cors

--- vm-template/file-server/src/index.js
+++ vm-template/file-server/src/index.js
@@ -3,13 +3,15 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fs = require('fs')
 const crypto = require('crypto')
+const cors = require('cors')
 
 
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors())
 
-const port = 4040
+const port = 4000
 
 app.post('/', (req, res) => {
   if(fs.existsSync(req.body.filepath)){
@@ -17,9 +19,8 @@ app.post('/', (req, res) => {
     const fileBuffer = fs.readFileSync(req.body.filepath)
     const hashSum = crypto.createHash('sha256')
     hashSum.update(fileBuffer)
-
     const hash = hashSum.digest('hex')
-    res.send({hash: hash})
+    res.send({check:true, hash: hash})
   }else{
     res.send({check: false, message: "File does not exists"})
   }
